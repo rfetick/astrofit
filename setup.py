fb8e0605a38386bbdbabb53fdd81dc34e47c2443
Romain JL. FETICK <32675073+rfetick@users.noreply.github.com>
2018-06-06 15:57:31 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 05 14:13:40 2018
+
+@author: rfetick
+"""
+
+from setuptools import setup, find_packages
+ 
+setup(name='astrofit',
+      version='1.0',
+      url='unknown',
+      license='GNU-GPL v3.0',
+      author='Romain JL Fetick',
+      description='Functions and tools for optics and fitting.',
+      packages=find_packages(exclude=['tests']),
+      install_requires=[
+          'numpy','math','mathplotlib','scipy'
+      ],
+      zip_safe=False,
+      )
