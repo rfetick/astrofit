1d4df533810e32812f108af5ce8304f7fe192e43
Romain JL. FETICK <32675073+rfetick@users.noreply.github.com>
2018-06-06 16:01:58 +0200

    Create __init__.py

---
+++ astrofit/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+ASTROFIT module
+Created on Tue Jun 05 14:12:55 2018
+
+Provides
+  1. Functions such as Gaussian and Moffat
+  2. Image centering
+  3. Circular library
+  4. Data fitting with model function
+  5. Zernike polynomials (class)
+
+@author: rfetick
+"""
+
+from .LSfit import *
+from .usual_functions import *
+from .circular_lib import *
+from .zernike import *
